9cfdbb755b803d82117407c5cc51ac734113b2e1
Mike Dilger <mike@mikedilger.com>
2021-05-04 21:17:39 +1200

    Define kernel_start()

--- src/kernel.rs
+++ src/kernel.rs
@@ -10,3 +10,10 @@ fn panic(_info: &core::panic::PanicInfo) -> !
 {
     crate::target::abort()
 }
+
+#[no_mangle]
+extern "C" fn kernel_start() {
+    // This initializes the kernel.
+
+    panic!("Not yet implemented.\n");
+}
